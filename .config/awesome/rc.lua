16a7918c42dea8d0199f5608b2ba59d13c874514
Trevor Bergeron <mallegonian@gmail.com>
2013-01-23 15:14:46 -0500

    moved terminal config to .Xresources

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -43,7 +43,7 @@ end
 beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm -bg black -fg green"
+terminal = "xterm"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
