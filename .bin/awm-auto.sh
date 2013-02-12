2570a9d674a54f88638eca6bec1f09e8e98b36f1
Trevor Bergeron <mallegonian@gmail.com>
2013-02-12 00:00:30 -0500

    Fix x11vnc (invert 'if')

--- .bin/awm-auto.sh
+++ .bin/awm-auto.sh
@@ -15,6 +15,8 @@ fi
 nitrogen --restore &
 
 if ps aux | grep x11vnc | grep -v grep >/dev/null ; then
+    echo -n
+else
     x11vnc -6 -alwaysshared -norepeat -usepw -many -remap Control_R-Super_L &
 fi
 
