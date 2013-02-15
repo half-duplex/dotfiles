4010ac166034ee08840d466e412f1866b972f6f7
Trevor Bergeron <mallegonian@gmail.com>
2013-02-15 15:10:33 -0500

    Made fair default awm layout

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -57,11 +57,11 @@ modkey = "Mod4"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 local layouts =
 {
+    awful.layout.suit.fair,
     awful.layout.suit.tile.bottom,
     awful.layout.suit.tile,
 --  awful.layout.suit.tile.left,
 --  awful.layout.suit.tile.top,
-    awful.layout.suit.fair,
 --  awful.layout.suit.fair.horizontal,
 --  awful.layout.suit.spiral,
 --  awful.layout.suit.spiral.dwindle,
