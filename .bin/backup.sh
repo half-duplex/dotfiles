dbac9b8a5678d60a6c6541c059ccf7eee32368e6
Trevor Bergeron <mal@sec.gd>
2014-03-15 16:57:03 -0400

    Transfer statistics and cleanup

--- .bin/backup.sh
+++ .bin/backup.sh
@@ -11,7 +11,7 @@ case $@ in
         exit
         ;;
     all)
-        STUFFTOBU="luca milo elliot mc-home mc-mc dream dream-sql" #maya =(
+        STUFFTOBU="luca milo eva nucacdf-web"
         ;;
     *)
         STUFFTOBU="$@"
@@ -20,34 +20,30 @@ esac
 
 START=$(date +%s)
 
-ARGS="-aXgoplE" # --stats" # --progress"
+ARGS="-aXgoplEh --info=progress2"
 BUDIR="/media/bulk1/backups"
 PARGS="--exclude-from /home/mal/.bin/backup.exclude.txt --delete"
+SSHARGS="-i /home/mal/.ssh/id_rsa"
 
 for butarget in $STUFFTOBU ; do
     echo -n "[Enter] to start $butarget:"
     read junk
     case $butarget in
         luca)
-            time rsync $ARGS / $BUDIR/luca $PARGS
+            rsync $ARGS / $BUDIR/luca $PARGS
+            echo "Backup from $(date '+%A, %d %B %Y, %T')" >$BUDIR/backupinfo.txt
             ;;
         milo)
-            time rsync $ARGS -e ssh root@milo:/ $BUDIR/milo $PARGS
+            rsync $ARGS -e "ssh $SSHARGS" root@milo:/ $BUDIR/milo $PARGS
+            echo "Backup from $(date '+%A, %d %B %Y, %T')" >$BUDIR/backupinfo.txt
             ;;
-        elliot)
-            time rsync $ARGS -e ssh root@elliot:/ $BUDIR/elliot $PARGS
+        eva)
+            rsync $ARGS -e "ssh $SSHARGS" root@eva:/ $BUDIR/eva $PARGS
+            echo "Backup from $(date '+%A, %d %B %Y, %T')" >$BUDIR/backupinfo.txt
             ;;
-        minecraft-home|mc-home)
-            time rsync $ARGS -e ssh minecraft@mc.bck.me:/opt/minecraft/ $BUDIR/minecraft/home --exclude={minemod/*,backups/*}
-            ;;
-        minecraft-mc|mc-mc)
-            time rsync $ARGS -e ssh minecraft@mc.bck.me:/opt/minecraft/backups/`date +%Y%m%d-`* $BUDIR/minecraft/minemod
-            ;;
-        dream)
-            time rsync $ARGS -e ssh mallegonian@dream:/home/mallegonian/ $BUDIR/dream
-            ;;
-        dream-sql)
-            mysqldump --all-databases -h mysql.blackcatstudios.org -u mallegonian -p >$BUDIR/dream-$START.sql
+        nucacdf-web)
+            rsync $ARGS -e "ssh $SSHARGS" mal@nucacdf.org:/home/mal/ $BUDIR/nucacdf-web
+            echo "Backup from $(date '+%A, %d %B %Y, %T')" >$BUDIR/backupinfo.txt
             ;;
         *)
             echo "Invalid backup target!"
@@ -60,5 +56,4 @@ echo " time: $(( ($FINISH-$START) / 60 )) minutes, $(( ($FINISH-$START) % 60 )) 
 
 df -h $BUDIR
 
-#touch $1/"Backup from $(date '+%A, %d %B %Y, %T')"
 
