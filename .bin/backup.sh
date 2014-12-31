1fd1be2fa200d6065de3a4828faa59f825c6ed09
Trevor Bergeron <mal@sec.gd>
2014-12-31 02:20:06 +0000

    Remove duplicate flags, add some

--- .bin/backup.sh
+++ .bin/backup.sh
@@ -20,9 +20,8 @@ esac
 
 START=$(date +%s)
 
-ARGS="-aXgoplEh --info=progress2"
+ARGS="-aXEhzAy --info=progress2 --delete --delete-delay --exclude-from /home/mal/.bin/backup.exclude.txt"
 BUDIR="/mnt/bulk/backups"
-PARGS="--exclude-from /home/mal/.bin/backup.exclude.txt --delete"
 SSHARGS="-i /home/mal/.ssh/id_rsa"
 
 for butarget in $STUFFTOBU ; do
@@ -30,21 +29,16 @@ for butarget in $STUFFTOBU ; do
     read junk
     case $butarget in
         luca)
-            rsync $ARGS / $BUDIR/luca $PARGS
+            rsync $ARGS / $BUDIR/luca
             echo "Backup from $(date '+%A, %d %B %Y, %T')" >$BUDIR/luca.backupinfo.txt
             ;;
         eva)
-            #rsync -4 $ARGS -e "ssh $SSHARGS" root@eva.sec.gd:/ $BUDIR/eva $PARGS
-            rsync -4 $ARGS -e "ssh $SSHARGS" root@10.8.0.1:/ $BUDIR/eva $PARGS
+            rsync -4 $ARGS -e "ssh $SSHARGS" root@eva.sec.gd:/ $BUDIR/eva
             echo "Backup from $(date '+%A, %d %B %Y, %T')" >$BUDIR/eva.backupinfo.txt
             ;;
         tron)
-            rsync -4 $ARGS -e "ssh $SSHARGS" root@tron.disruptivelabs.org:/ $BUDIR/tron $PARGS
+            rsync -4 $ARGS -e "ssh $SSHARGS" root@tron.disruptivelabs.org:/ $BUDIR/tron
             echo "Backup from $(date '+%A, %d %B %Y, %T')" >$BUDIR/tron.backupinfo.txt
-            ;;
-        nucacdf-web)
-            rsync $ARGS -e "ssh $SSHARGS" mal@nucacdf.org:/home/mal/ $BUDIR/nucacdf-web
-            echo "Backup from $(date '+%A, %d %B %Y, %T')" >$BUDIR/nucacdf-web.backupinfo.txt
             ;;
         *)
             echo "Invalid backup target!"
@@ -56,5 +50,4 @@ FINISH=$(date +%s)
 echo " time: $(( ($FINISH-$START) / 60 )) minutes, $(( ($FINISH-$START) % 60 )) seconds"
 
 df -h $BUDIR
-
 
